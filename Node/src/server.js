229db8bb9cd9b32bd3c64f60466990844f2320d7
Caetano Neto <caetano.n@gmail.com>
2026-01-10 23:42:58 -0300

    server.js

--- Node/src/server.js
+++ Node/src/server.js
@@ -1,1 +1,13 @@
-console.log('teste')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "Hello world"
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is up at http://localhost:${PORT}`)
+})
